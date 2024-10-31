b816a081ba38b19f2e74bfd799f27d21d70c5a0c
Пользователь <tu7c1ne72@code.edu.az>
2024-10-31 22:02:00 +0400

    Bots

---
+++ bot/bot.js
@@ -0,0 +1,34 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const BOT_TOKEN=''
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+bot.onText(/\/echo/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = 'sa';
+    bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/start/, async (msg, match) => {
+    const chatId = msg.chat.id;
+
+    try {
+        const userProfilePhotos = await bot.getUserProfilePhotos(msg.from.id);
+        if (userProfilePhotos.total_count > 0) {
+            const fileId = userProfilePhotos.photos[0][0].file_id;
+
+            // Send the photo directly using the file ID
+            await bot.sendPhoto(chatId, fileId, { caption: 'Here is your profile picture.' });
+        } else {
+            await bot.sendMessage(chatId, 'No profile picture found.');
+        }
+    } catch (error) {
+        console.error('Error fetching profile picture:', error.response ? error.response.body : error.message);
+
+        await bot.sendMessage(chatId, 'There was an error fetching your profile picture.');
+    }
+
+    bot.sendMessage(chatId, msg.from.id);
+});
+
+module.exports = bot;
